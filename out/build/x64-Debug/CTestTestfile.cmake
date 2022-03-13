1f1be79be79e0d7151c30c8b29326f36e529b972
naomipappe <naomipappe.zvieriev@gmail.com>
2022-03-13 22:18:55 +0200

    Deleting a rogue directory

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Uroboros
-# Build directory: E:/Uroboros/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
